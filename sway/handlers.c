5f2a2d5d5fa4d8f0626ed49205411ac6b2bdd664
Drew DeVault <ddevault@linode.com>
2016-06-17 13:54:08 -0400

    Fix view on fake workspace being visible

--- sway/handlers.c
+++ sway/handlers.c
@@ -412,7 +412,6 @@ static bool handle_view_created(wlc_handle handle) {
 		list_add(output->unmanaged, h);
 		wlc_view_set_mask(handle, VISIBLE);
 	}
-	wlc_view_set_mask(handle, VISIBLE);
 
 	if (return_to_workspace && current_ws != swayc_active_workspace()) {
 		// we were on one workspace, switched to another to add this view,
